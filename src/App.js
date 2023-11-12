690a7b81067fefb9e648ed95d058da61c246b89a
Douglas <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-11-12 17:43:07 -0300

    update telaLogin

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Contact from './pages/Contact';
 import OurStore from './pages/OurStore';
 import Blogs from './pages/Blogs';
 import CompareProduct from './pages/CompareProduct';
+import Wishlist from './pages/Wishlist';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
             <Route path='store' element={<OurStore />} />
             <Route path='blogs' element={<Blogs />} />
             <Route path='compare-product' element={<CompareProduct />} />
+            <Route path='wishlist' element={<Wishlist />} />
+            <Route path='login' element={<Login />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      sim
+    </div>
+  )
+}
+
+export default Signup
